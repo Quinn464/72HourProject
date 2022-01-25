6f749fdf9a81e7be764932d32a56468376a53b30
Fabiola Aguilera <fabiaguilera96@gmail.com>
2022-01-24 19:35:38 -0500

    fabi branch and post reply class

---
+++ ElevenNote.Data/PawstReply.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    public class PawstReply // post a reply
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public Guid OwnerId { get; set; }
+    }
+}
