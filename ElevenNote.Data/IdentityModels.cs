9cde284c45cfee3e73d8d625917fbbdecf6be125
Quinn464 <93061089+Quinn464@users.noreply.github.com>
2022-01-24 19:18:44 -0500

    dev

--- ElevenNote.Data/IdentityModels.cs
+++ ElevenNote.Data/IdentityModels.cs
@@ -14,6 +14,7 @@ using System.Web;
 namespace Meow.Data
 
 {
+    //dev
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
